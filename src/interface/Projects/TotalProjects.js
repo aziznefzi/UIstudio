187d2ae1619a96f0c4c00a197197444876a62369
aziznefzi <nefziaziz763gmail.com>
2026-01-20 22:57:21 +0100

    Product Button Update

--- src/interface/Projects/TotalProjects.js
+++ src/interface/Projects/TotalProjects.js
@@ -6,6 +6,5 @@ export const TotalProjects = [
    {id: 1, imageProject: rostro , NameProject: "Rostro", SkelsProject: "Reactjs, Node js, MongoDB", GitLink: "https://github.com/aziznefzi/restro.git", ProjectLink: "https://restro-peach-ten.vercel.app/" },
    {id: 2, imageProject: PhoneStore , NameProject: "X-Phone", SkelsProject: "React Js", GitLink: "https://github.com/aziznefzi/X-Phone.git", ProjectLink: "https://x-phone.vercel.app/" },
    {id: 3, imageProject: ClothingStor , NameProject: "Clothing Stor", SkelsProject: "Node js, MongoDB", GitLink: "https://github.com/aziznefzi/clothing-store-Project.git", ProjectLink: "https://clothing-store-project-git-main-aziz-nefzis-projects.vercel.app/" },
-   {id: 4, imageProject: Croud , NameProject: "Managment System", SkelsProject: "React Js, Node Js", },
-
+   {id: 4, imageProject: Croud , NameProject: "Managment System", SkelsProject: "React Js, Node Js", GitLink: "https://github.com/aziznefzi/Management-System.git", ProjectLink: "https://management-system-sable-chi.vercel.app/"},
 ] 
